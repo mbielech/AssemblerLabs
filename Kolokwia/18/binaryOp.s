14835bd097530b26868e42657df48a88a0e1876d
Maciej Bielech <maciej.bielech@gmail.com>
2018-06-14 17:41:15 +0200

    Binary operation repaired

--- Kolokwia/18/binaryOp.s
+++ Kolokwia/18/binaryOp.s
@@ -9,7 +9,7 @@
 
 binA:
 	
-# -------------- dlugosc arg1 --------------------
+# -------------- dlugosc arg1 --------------------------------
 
 	XOR	%r15,	%r15	# licznik
 
@@ -26,15 +26,43 @@ end1:
 
 	MOV	%r15,	%r14	# lenght of arg1
 	
-# END----------- dlugosc arg1 --------------------
+# END----------- dlugosc arg1 --------------------------------
+	
+	MOV	%r14,	%r10	# dlugosc arg1
 
-# -------------- dlugosc arg2 --------------------
+# -------------- utworzenie binarnej liczby z arg1 -----------
+
+	XOR	%r12,	%r12	# miejsce na liczbe binarna
+	MOV	$1,		%r13	# maska
+	DEC	%r14			# wskaznik na ostatni element
+	
+nextChar1:
+	
+	MOV	(%rsi,%r14,1),	%r15b	# znak
+	
+	CMP	$'1',			%r15b	# czy znak to 1
+	JNE	not11
+	
+	OR	%r13,	%r12	# ustaw dany bit
+	
+not11:
+
+	SHL	$1,		%r13	# przesun maske
+	DEC	%r14			# wskaznik na poprzedni char
+	CMP	$0,		%r14	# czy juz koniec?
+	JGE	nextChar1	
+
+# END----------- utworzenie binarnej liczby z arg1 -----------
+	
+	MOV	%r12,	%r8		# arg1 binarnie
+
+# -------------- dlugosc arg2 --------------------------------
 
 	XOR	%r15,	%r15	# licznik
 
 next2:	
 
-	MOV	(%rdx,%r15,1),	%r9b	# arg1[i]
+	MOV	(%rdx,%r15,1),	%r9b	# arg2[i]
 	CMP	$0,				%r9b	# if( == NULL)
 	JE 	end2
 	
@@ -43,142 +71,138 @@ next2:
 	
 end2:
 
-	MOV	%r15,	%r13	# lenght of arg2
+	MOV	%r15,	%r14	# lenght of arg2
 	
-# END----------- dlugosc arg2 --------------------
+# END----------- dlugosc arg2 --------------------------------
+	
+	MOV	%r14,	%r11	# dlugosc arg1
 
-	XOR	%r15,	%r15	# ilosc operacji z zerami
-	CMP	%r13,	%r14	# if(len(arg1)==len(arg2))
-	JE	rowne
-	JMP	nierowne
-rowne:
-	MOV	%rsi,	%r13	# rowne
-	MOV	%rdx,	%r14	# rowne
-	JMP allOk
-	
-nierowne:
-	
-	JG	lenArg1Greater
-	
-	MOV	%r13,	%r15
-	SUB	%r14,	%r15
-	
-	MOV	%rsi,	%r13	# mniejsze
-	MOV	%rdx,	%r14	# wieksze
-	
-	JMP	allOk
-	
-lenArg1Greater:
-	
-	MOV	%r14,	%r15
-	SUB	%r13,	%r15
-	
-	MOV	%rdx,	%r13	# mniejsze
-	MOV	%rsi,	%r14	# wieksze
-	
-allOk:
-	XOR	%r8,	%r8		# i = 0
-	XOR	%r9,	%r9		# just 0
-	ADD	$'0',	%r9
+# -------------- utworzenie binarnej liczby z arg2 -----------
 
-next:
-	CMP	$0,	%r15
-	JE	endnext
+	XOR	%r12,	%r12	# miejsce na liczbe binarna
+	MOV	$1,		%r13	# maska
+	DEC	%r14			# wskaznik na ostatni element
+	
+nextChar2:
+	
+	MOV	(%rdx,%r14,1),	%r15b	# znak
+	
+	CMP	$'1',			%r15b	# czy znak to 1
+	JNE	not12
+	
+	OR	%r13,	%r12	# ustaw dany bit
+	
+not12:
+
+	SHL	$1,		%r13	# przesun maske
+	DEC	%r14			# wskaznik na poprzedni char
+	CMP	$0,		%r14	# czy juz koniec?
+	JGE	nextChar2	
+
+# END----------- utworzenie binarnej liczby z arg2 -----------
+
+	MOV	%r12,	%r9		# arg2 binarnie
+	
+# -------------- znalezienie wiekszej dlugosci ---------------
+
+	CMP	%r10,	%r11
+	JL	r10wieksze
+	
+	MOV	%r11,	%r10
+	
+r10wieksze:
+	
+	
+# END----------- znalezienie wiekszej dlugosci ---------------
+	
+# r8 - arg1
+# r9 - arg2	
+# r10 - dlugosc dluzszego napisu
+
+	
+
+# -------------- operacja 0 - dodawanie ----------------------
 	
 	CMP	$0,		%rcx
-	JNE	opNot0
+	JNE	notADD
 	
-	MOV	(%r14,%r8,1),	%r10b
-	MOV	%r10b,			(%rdi,%r8,1)
-	
-opNot0:
+	ADD	%r8,	%r9
+
+notADD:
+
+# END----------- operacja 0 - dodawanie ----------------------
+
+# -------------- operacja 1 - and ----------------------
 	
 	CMP	$1,		%rcx
-	JNE	opNot1
+	JNE	notAND
 	
-	MOV	%r9b,			(%rdi,%r8,1)
-	
-opNot1:
+	AND	%r8,	%r9
+
+notAND:
+
+# END----------- operacja 1 - and ----------------------
+
+# -------------- operacja 2 - or ----------------------
 	
 	CMP	$2,		%rcx
-	JNE	opNot2
+	JNE	notOR
 	
-	MOV	(%r14,%r8,1),	%r10b
-	MOV	%r10b,			(%rdi,%r8,1)
-	
-opNot2:
+	OR	%r8,	%r9
 
+notOR:
+
+# END----------- operacja 2 - or ----------------------
+
+# -------------- operacja 3 - xor ----------------------
+	
 	CMP	$3,		%rcx
-	JNE	opNot3
+	JNE	notXOR
 	
-	MOV	(%r14,%r8,1),	%r10b
-	MOV	%r10b,			(%rdi,%r8,1)
-	
-opNot3:
-	
-	INC	%r8
-	DEC	%r15
-	JMP next
-	
-endnext:
-	
-# r8 - licznik dla dluzszego r14
-# r9 - licznik dla krotszego r13
-	XOR	%r9,	%r9
+	XOR	%r8,	%r9
 
-nextWhile:
+notXOR:
 
-	MOV	(%r14,%r8,1),	%r15b
-	CMP	$0,				%r15b
-	JE end
-	
-	MOV	(%r13,%r9,1),	%r12b
-	
-	SUB	$'0',			%r15b
-	SUB	$'0',			%r12b
-	
-	CMP	$0,		%rcx
-	JNE	optNot0
-	
-	ADD	%r15b,			%r12b
-	ADD	$'0',			%r12b
-	MOV	%r12b,			(%rdi,%r8,1)
-	
-optNot0:
-	
-	CMP	$1,		%rcx
-	JNE	optNot1
-	
-	AND	%r15b,			%r12b
-	ADD	$'0',			%r12b
-	MOV	%r12b,			(%rdi,%r8,1)
-	
-optNot1:
-	
-	CMP	$2,		%rcx
-	JNE	optNot2
-	
-	OR	%r15b,			%r12b
-	ADD	$'0',			%r12b
-	MOV	%r12b,			(%rdi,%r8,1)
-	
-optNot2:
+# END----------- operacja 3 - xor ----------------------
 
-	CMP	$3,		%rcx
-	JNE	optNot3
+# %rdi - char* result
+# r9 wynik operacji
+# r10 - dlugosc dluzszego napisu
+
 	
-	XOR	%r15b,			%r12b
-	ADD	$'0',			%r12b
-	MOV	%r12b,			(%rdi,%r8,1)
 	
-optNot3:
+	MOVB $0,	(%rdi,%r10,1)	# '\0' na koniec napisu	
+
+# -------------- tworzenie napisu result ---------------------
 	
-	INC	%r8
-	INC	%r9
-	JMP nextWhile
+	DEC	%r10			# wskaznik na ostatni element
+	MOV	$1,		%r13	# maska
+	MOV	$'0',	%r8b	# char '0'
+	MOV	$'1',	%r11b	# char '1'
 	
-end:
+nextResult:
+	
+	MOV	%r9,	%r15	# kopia wyniku
+	AND	%r13,	%r15	# operacja and
+	CMP	%r13,	%r15	# czy w wyniku byl ustawiony ten bit?
+	JNE zeroResult
+	
+	MOV	%r11b,	(%rdi,%r10,1)	# wstaw 1
+	JMP	nieWstaw0
+	
+zeroResult:
+	
+	MOV	%r8b,	(%rdi,%r10,1)	# wstaw 0
+
+nieWstaw0:
+	
+	SHL	$1,		%r13
+	DEC	%r10
+	CMP	$0,		%r10	# czy juz koniec?
+	JGE	nextResult
+
+# END----------- tworzenie napisu result ---------------------
+
 	MOV	%rdi,	%rax
 	RET
-	
 
