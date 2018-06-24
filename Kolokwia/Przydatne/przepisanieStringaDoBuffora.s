cdf5595598352057cd6bb6f8cc127a07848789f6
Maciej Bielech <maciej.bielech@gmail.com>
2018-06-24 10:38:22 +0200

    New table syntax with index

--- Kolokwia/Przydatne/przepisanieStringaDoBuffora.s
+++ Kolokwia/Przydatne/przepisanieStringaDoBuffora.s
@@ -5,23 +5,21 @@
 #rsi a
  
 funa:
-	
-	MOV	%rdi,	%r12	# buffor
-	MOV	%rsi,	%r11	# a
+	XOR	%r15,			%r15			# licznik = 0
 
 next:
+	
+	MOV	(%rsi,%r15,1),	%r9b			# zapisz znak z a do tmp
 
-	CMP $0,		(%r11)	# if(a[i]!=0)
+	CMP	$0,				%r9b			# if(a[i]!=0)
 	JE	end
 	
-	MOV	(%r11), %r9b	# zapisz znak z a do tmp
-	MOV %r9b,	(%r12)	# zapisz znak z tmp do buffer
+	
+	MOV %r9b,			(%rdi,%r15,1)	# zapisz znak z tmp do buffer
 
-	INC	%r11			# a* = (a+1)*
-	INC %r12			# buffor* = (buffor+1)*
-
+	INC	%r15							# licznik++
     JMP next
 
 end:
-	MOV	%rdi,	%rax
+	MOV	%rdi,			%rax
 	RET
